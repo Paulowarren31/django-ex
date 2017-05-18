02d88431ef6ad558971c8497cc1c82852c707be3
paulowar <paulowar@umich.edu>
2017-05-18 11:37:40 -0400

    workin on it pls

--- welcome/views.py
+++ welcome/views.py
@@ -7,15 +7,17 @@ from . import database
 from .models import PageView
 
 import requests
+import base64
 
 
 
 # Create your views here.
 
 def index(request):
-  f = open('/usr/src/app/django-example/local/saml/key', 'r')
+  with open('/usr/src/app/django-example/local/saml/key', 'rb') as f:
+    string = str(base64.b64encode(f.read()), 'utf-8')
   #r = requests.get('https://api.darksky.net/forecast/'+os.environ['KEY']+'/42.280826,-83.743038')
-  return render(request, 'index.html', {'weatherString': 'asd', 'key': f})
+  return render(request, 'index.html', {'weatherString': 'asd', 'key': string})
 
 def health(request):
   return HttpResponse(PageView.objects.count())
