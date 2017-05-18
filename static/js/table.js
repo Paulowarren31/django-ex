04ffd39f7cb1d618b160b1c49001b56bd069b937
paulowar <paulowar@umich.edu>
2017-05-18 16:33:13 -0400

    added some jquery for table manipulation

--- static/js/table.js
+++ static/js/table.js
@@ -3,19 +3,35 @@ $(function(){
   accounts.on('click', function(e){
     acc_pk = e.target.parentNode.id.split('-')[1]
 
+    //get all the categories associated with the account
     categories = $('[id^=acc-'+acc_pk+'][id$=cat]').toggleClass('hidden')
-    //get all categories
+
+
+    //when toggling off, we wanna also hide the items
+    if(categories.hasClass('hidden')){
+
+      //get all items belonging to the categories above
+      items = $('[id^=acc-'+acc_pk+'][id$=i]')
+      items.addClass('hidden')
+    }
+
+
+    //toggle triangle icon
+    $('#acc-i-'+acc_pk).toggleClass('fa-caret-down fa-caret-up')
   })
 
   categories = $('[id^=acc][id$=cat]')
-  console.log(categories)
 
+  //on category click, show all items under that category
   categories.on('click', function(e){
     acc_pk = e.target.parentNode.id.split('-')[1]
     cat_pk = e.target.parentNode.id.split('-')[3]
 
-    items = $('[id^=acc-'+acc_pk+'-cat-'+cat_pk+']').not('[id$=cat]').toggleClass('hidden')
-    console.log(items)
+    //toggle hidden class for all items under a category
+    $('[id^=acc-'+acc_pk+'-cat-'+cat_pk+']').not('[id$=cat]').toggleClass('hidden')
+
+    $('#cat-i-'+cat_pk).toggleClass('fa-caret-down fa-caret-up')
+
   })
 
 })
