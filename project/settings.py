39261cbc4f77144c641f41fc4276db5d0b027035
paulowar <paulowar@umich.edu>
2017-05-23 16:19:35 -0400

    changing docker-compose dont think it did anything

--- project/settings.py
+++ project/settings.py
@@ -87,13 +87,8 @@ WSGI_APPLICATION = 'wsgi.application'
 from . import database
 
 DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': 'postgres',
-      'USER': 'postgres',
-      'HOST': 'db',
-      'PORT': 5432,
-      }
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 
 
