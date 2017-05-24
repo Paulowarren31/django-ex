79696c746c0d316b43b2bb6db905942eb48c9064
paulowar <paulowar@umich.edu>
2017-05-24 10:21:31 -0400

    db config wrong

--- project/settings.py
+++ project/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'wsgi.application'
 from . import database
 
 DATABASES = {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
     }
 
 
