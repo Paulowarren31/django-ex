8d78cfd173838ad6477c86c7036533b4b5ca9852
paulowar <paulowar@umich.edu>
2017-06-12 11:25:07 -0400

    adding more select fields

--- welcome/views.py
+++ welcome/views.py
@@ -18,11 +18,16 @@ connection_string = 'paulowar/Pw6517nP@pinntst.dsc.umich.edu:1521/pinndev.world'
 def index(request):
   conn = cx_Oracle.connect(connection_string)
   cursor = conn.cursor()
+
   query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_bud_seq)"
 
-  cursor.execute(query)
+  budget_seq = cursor.execute(query)
 
-  return render(request, 'index.html', {'cursor': cursor})
+  query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_grp_vp_area)"
+
+  vp_area = cursor.execute(query)
+
+  return render(request, 'index.html', {'bud_seq': budget_seq, 'vp': vp_area})
 
 #def table(request):
 #  accounts = ExpenseAccount.objects.all()
