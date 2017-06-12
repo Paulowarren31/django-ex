682d1060f8516e25ffd427081878526cdccf7864
paulowar <paulowar@umich.edu>
2017-06-12 11:40:46 -0400

    trying multiple queries

--- db_tests.py
+++ db_tests.py
@@ -5,9 +5,21 @@ connection_string = 'paulowar/Pw6517nP@pinntst.dsc.umich.edu:1521/pinndev.world'
 conn = cx_Oracle.connect(connection_string)
 cursor = conn.cursor()
 
+query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_grp_vp_area)"
+
+vp_area = cursor.execute(query)
+
+for row in  vp_area:
+  print row
+
+print 'ASDASDASDJJ'
+
 query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_bud_seq)"
 
-cursor.execute(query)
+budget_seq = cursor.execute(query)
 
-for row in cursor:
-  print row[17]
+for row in  budget_seq:
+  print row
+
+
+
--- welcome/views.py
+++ welcome/views.py
@@ -16,18 +16,24 @@ connection_string = 'paulowar/Pw6517nP@pinntst.dsc.umich.edu:1521/pinndev.world'
 
 @login_required(login_url='/accounts/login')
 def index(request):
+  dic = {}
+
   conn = cx_Oracle.connect(connection_string)
   cursor = conn.cursor()
-
-  query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_bud_seq)"
-
-  budget_seq = cursor.execute(query)
 
   query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_grp_vp_area)"
 
   vp_area = cursor.execute(query)
 
-  return render(request, 'index.html', {'bud_seq': budget_seq, 'vp': vp_area})
+  dic['vp'] = vp_area
+
+  query = "select * from um_ecomm_dept_units_rept where ROWID IN ( SELECT MAX(ROWID) FROM um_ecomm_dept_units_rept GROUP BY dept_bud_seq)"
+
+  budget_seq = cursor.execute(query)
+
+  dic['bud_seq'] = budget_seq
+
+  return render(request, 'index.html', dic)
 
 #def table(request):
 #  accounts = ExpenseAccount.objects.all()
